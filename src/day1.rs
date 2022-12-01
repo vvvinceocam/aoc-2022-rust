27de59195f126a6b8d01e61c9d4ea751f35e4824
vvvince <vvvince@ofcompute.rs>
2022-12-01 12:22:50 +0100

    improve tests and code

--- src/day1.rs
+++ src/day1.rs
@@ -5,9 +5,9 @@ pub fn input_generator(input: &str) -> Vec<Vec<u32>> {
     input
         .split("\n\n")
         .map(|elf| {
-            elf.split("\n")
+            elf.split('\n')
                 .map(|cal| cal.parse::<u32>().unwrap())
-                .collect::<Vec<_>>()
+                .collect()
         })
         .collect()
 }
@@ -25,31 +25,28 @@ pub fn solve_part2(input: &[Vec<u32>]) -> u32 {
 
 #[cfg(test)]
 mod tests {
-    use super::{solve_part1, solve_part2};
+    use super::{input_generator, solve_part1, solve_part2};
+    static INPUT: &str = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
 
     #[test]
-    fn valid_solver_part1() {
-        let input = vec![
+    fn input_generator_build_vec() {
+        let expect = vec![
             vec![1000, 2000, 3000],
             vec![4000],
             vec![5000, 6000],
             vec![7000, 8000, 9000],
             vec![10_000],
         ];
-
-        assert_eq!(solve_part1(&input), 24_000);
+        assert_eq!(input_generator(INPUT), expect);
     }
 
     #[test]
-    fn valid_solver_part2() {
-        let input = vec![
-            vec![1000, 2000, 3000],
-            vec![4000],
-            vec![5000, 6000],
-            vec![7000, 8000, 9000],
-            vec![10_000],
-        ];
+    fn solver_part1_match_example() {
+        assert_eq!(solve_part1(&input_generator(INPUT)), 24_000);
+    }
 
-        assert_eq!(solve_part2(&input), 45_000);
+    #[test]
+    fn solver_part2_match_example() {
+        assert_eq!(solve_part2(&input_generator(INPUT)), 45_000);
     }
 }

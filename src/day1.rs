ce9c1fa68888e4ad0cc1ce9921b948e348202a31
vvvince <vvvince@ofcompute.rs>
2022-12-02 18:29:13 +0100

    remove intermediate variable in day1 p2

--- src/day1.rs
+++ src/day1.rs
@@ -19,17 +19,21 @@ pub fn solve_part1(input: &[Vec<u32>]) -> u32 {
 
 #[aoc(day1, part2)]
 pub fn solve_part2(input: &[Vec<u32>]) -> u32 {
-    let mut elves = input.iter().map(|elf| elf.iter().sum()).collect::<Vec<u32>>();
-    elves.select_nth_unstable_by(3, |a, b| b.cmp(a)).0.iter().sum()
+    input.iter()
+        .map(|elf| elf.iter().sum())
+        .collect::<Vec<u32>>()
+        .select_nth_unstable_by(3, |a, b| b.cmp(a))
+        .0.iter().sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::{input_generator, solve_part1, solve_part2};
+
     static INPUT: &str = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
 
     #[test]
-    fn input_generator_build_vec() {
+    fn input_generator_builds_vec() {
         let expect = vec![
             vec![1000, 2000, 3000],
             vec![4000],
